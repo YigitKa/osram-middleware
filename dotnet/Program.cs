c282db22a8a89216d890410d1df9c77da65a9039
ykatircioglu <90yigit@gmail.com>
2022-10-11 15:33:29 +0300

    first request tested with dotnet

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+HttpClient httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("https://api-vlrg.osram.info/cars/");
+HttpResponseMessage responseMessage = httpClient.GetAsync("lookups?kind_id=1&lang=en&lookuptype=manufacturers").Result;
+Console.WriteLine($"Status Code: {responseMessage.StatusCode}");
+
+if (responseMessage.StatusCode != System.Net.HttpStatusCode.OK)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+}
+Console.WriteLine(responseMessage.Content.ReadAsStringAsync().Result);
+
+
+
+
