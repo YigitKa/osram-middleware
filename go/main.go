6082d181b9075e35759b24a4f89223fe415c9874
ykatircioglu <90yigit@gmail.com>
2022-10-17 23:18:41 +0300

    http packages added.

--- go/main.go
+++ go/main.go
@@ -1,9 +1,22 @@
-package osram
+package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "try to see this text on browser!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
 
 func main() {
 	fmt.Printf("why there is default no hello world")
+	handleRequests()
 }
