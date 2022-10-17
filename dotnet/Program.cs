8e74ed64f7cb07e673aafa1c411e5d1b2b3a0b1a
ykatircioglu <90yigit@gmail.com>
2022-10-17 23:07:50 +0300

    warm up

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,6 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using dotnet.Model;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// get data
 HttpClient httpClient = new HttpClient();
 httpClient.BaseAddress = new Uri("https://api-vlrg.osram.info/cars/");
 HttpResponseMessage responseMessage = httpClient.GetAsync("lookups?kind_id=1&lang=en&lookuptype=manufacturers").Result;
@@ -10,8 +13,16 @@ if (responseMessage.StatusCode != System.Net.HttpStatusCode.OK)
 {
     Console.ForegroundColor = ConsoleColor.Red;
 }
-Console.WriteLine(responseMessage.Content.ReadAsStringAsync().Result);
 
-
+Manufacturer[]? manufacturers = JsonSerializer.Deserialize<Manufacturer[]>(responseMessage.Content.ReadAsStringAsync().Result);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Araç marka sayisi: {manufacturers?.Count()}");
+Console.WriteLine($"********************************************");
+Console.WriteLine($"*   id|Name     *");
+foreach (Manufacturer manufacturer in manufacturers)
+{
+    Console.WriteLine($"*   {manufacturer.id}:{manufacturer.name}   *");
+}
+Console.WriteLine($"********************************************");
 
 
